c5416fe19a8a70920eb64f1aac8369ebfc55c76f
Babin Bohora <bohorababin1@gmail.com>
2022-04-09 01:47:05 +0900

    basic template for rails app done with dependencies

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h3>Hello from baba React</h3>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/index.jsx
@@ -0,0 +1,22 @@
+import '../assets/stylesheets/application.scss';
+
+import React from 'react';
+import ReactDom from 'react-dom/client';
+
+import App from './components/App'
+
+const root = document.getElementById('root');
+
+ReactDom.createRoot(root).render(< App />)
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// import '../assets/stylesheets/application.scss';
+
+// import App from './components/App';
+
+// const root = document.getElementById('root');
+// if (root) {
+//   ReactDOM.render(<App />, root);
+// }
