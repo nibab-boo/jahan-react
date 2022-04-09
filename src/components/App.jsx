e9cdd8342f257962983c1c2c10ce76f3f8b88490
Babin Bohora <bohorababin1@gmail.com>
2022-04-10 02:48:58 +0900

    images added and photo-gallery added. Media query not working though

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,28 @@
+import { Carousel } from 'bootstrap';
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 // importing from components
 import Header from './Header';
-
+import HomeCarousel from './HomeCarousel';
+import PhotoGallery from './PhotoGallery';
 
 const App = () => {
   return (
     <>
-      < Header />
+      <Router>
+        < Header />
+        <Routes>
+          <Route exact path="/" element= {
+            <>
+              < HomeCarousel />
+              < PhotoGallery />
+            </>
+          } >
+          </Route>
+        </Routes>
+      </Router>
     </>
   );
 };
