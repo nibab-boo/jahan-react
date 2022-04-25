6a9617747a86972531b7be91ae15c7ca5a64f1a2
Babin Bohora <bohorababin1@gmail.com>
2022-04-25 19:49:56 +0900

    Language Provider done

---
+++ src/components/LanguageProvider.jsx
@@ -0,0 +1,42 @@
+import React, {useState, useEffect, useContext, createContext} from 'react';
+
+import { jsonEn } from './json_en';
+import { jsonJp } from './json_jp';
+
+const LanguageContext = createContext();
+const LanguageUpdateContext = createContext();
+
+export const useLanContext = () => {
+  return useContext(LanguageContext);
+}
+export const useLanUpdateContext = () => {
+  return useContext(LanguageUpdateContext);
+}
+
+const LanguageProvider = ({children}) => {
+  const [enLan, setEnLan] = useState();
+
+  const [json, setJson] = useState(jsonJp);
+
+  useEffect(() => {
+    if (enLan) {
+      setJson(jsonEn);
+    } else {
+      setJson(jsonJp);
+    }
+  }, [enLan]);
+  const toggleLan = (newLan) => {
+    setEnLan(newLan);
+  };
+
+  return (
+    <LanguageContext.Provider value={json}>
+      <LanguageUpdateContext.Provider value={toggleLan}>
+
+        {children}
+      </LanguageUpdateContext.Provider>
+    </LanguageContext.Provider>
+  );
+};
+
+export { LanguageProvider };
