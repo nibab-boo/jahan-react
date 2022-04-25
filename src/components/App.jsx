c4429cc6650ce50640e14ff94bb6183e3b04a60c
Babin Bohora <bohorababin1@gmail.com>
2022-04-25 20:04:02 +0900

    app done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
-// import { Carousel } from 'bootstrap';
-import React from 'react';
+import React, {useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// import './assets/stylesheets/application.css'
 
 // importing from components
 import Header from './Header';
@@ -9,38 +10,52 @@ import PhotoGallery from './PhotoGallery';
 import Contact from './Contact';
 import Footer from "./Footer";
 import Menu from "./Menu";
+import Blog from "./Blog";
+import { LanguageProvider } from './LanguageProvider';
 
-const App = () => {
+function App() {
+  const [blogs, setBlogs] = useState();
+  useEffect(() => {
+    fetch("/api/v1/blogs")
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data);
+      setBlogs(data);
+    })
+  }, [])
+
   return (
-    <>
+    <LanguageProvider>
       <Router>
         <Header />
-        <Routes>
-          <Route
-            exact
-            path="/"
-            element={(
-              <>
-                <HomeCarousel />
-                <PhotoGallery />
-                <Contact />
-              </>
+         <Routes>
+          <Route exact path="/" element={(
+            <> 
+              <HomeCarousel />
+              <PhotoGallery />
+              <Contact />
+            </>
           )}
-          />
-          <Route
-            path="/menu"
-            element={(
-              <>
-                <Menu />
-                {/* < Menu /> */}
-              </>
-          )}
-          />
+          >
+          </Route>
+          
+          <Route path="/menu" element={(
+            <>
+              <Menu />
+            </>
+          )}>
+          </Route>
+          <Route path="/blog" element={
+            <>
+              < Blog blogs={ blogs } />
+            </>
+          }>
+          </Route>
         </Routes>
         <Footer />
       </Router>
-    </>
+    </LanguageProvider>
   );
-};
+}
 
 export default App;
