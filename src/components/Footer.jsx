9b3c77c1fd900f7a684439a418ef66d54a0ad1b0
Babin Bohora <bohorababin1@gmail.com>
2022-04-25 20:18:54 +0900

    Footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,34 +1,27 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
+import { useLanContext } from './LanguageProvider';
 
 const Footer = () => {
+  const json = useLanContext();
+  const { footer } = json;
   return (
-    <div className='main-footer' style={{background: "#8E3C1D"}}>
-      <h4 className='nav-jahan'>Reach out to</h4>
+    <div className="main-footer" style={{ background: "#8E3C1D" }}>
+      <h4 className="nav-jahan">{footer.name}</h4>
       <div className="footer-box d-flex justify-content-evenly">
-        <div className="footer-connect">
-          <h5 className='nav-jahan'>Connect</h5>
-          <ul>
-            <li><a href="">Instagram</a></li>
-            <li><a href="">Twitter</a></li>
-            <li><a href="">Facebook</a></li>
-          </ul>
-        </div>
-        <div className="footer-order">
-          <h5 className='nav-jahan'>Order</h5>
-          <ul>
-            <li><a href="tel:0489155644">Phone</a></li>
-            <li><a href="">Uber!</a></li>
-            <li><a href="">Demaekan</a></li>
-          </ul>
-        </div>
-        <div className="footer-reserve">
-          <h5 className='nav-jahan'>Reservations</h5>
-          <ul>
-            <li><a href="tel:0489155644">Phone</a></li>
-            <li><a href="">Hotpepper!</a></li>
-            <li><a href="">Yahooooo!</a></li>
-          </ul>
-        </div>
+        { footer.subList.map((list) => {
+          return (
+
+            <div className="footer-name" key={list.name}>
+              <h5 className="nav-jahan">{list.name}</h5>
+              <ul>
+                {list.items.map((item) => {
+                  return <li key={item.item}><a href={`${item.link}`}>{item.item}</a></li>;
+                })}
+              </ul>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
