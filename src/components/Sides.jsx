4b49b6d17bdac87fb0a72d40f5e8f4e4d2c3e775
Babin Bohora <bohorababin1@gmail.com>
2022-04-25 20:38:36 +0900

    sides refactoring done"

--- src/components/Sides.jsx
+++ src/components/Sides.jsx
@@ -1,96 +1,32 @@
 import React from 'react';
 import { Tabs, Tab } from 'react-bootstrap';
 
-import cheeseNan from "./../../assets/images/cheese_nan.jpeg"
-
-const Sides = () => {
+const Sides = ({ sides }) => {
+  const tabList = ["Tandoori", "Salad", "Appetizer"];
   return (
     <>
-      <Tabs defaultActiveKey="Tandoori" id="menu-sub-tab" className="mb-3">
-        <Tab eventKey="Tandoori" title="Tandoori">
-          <div className='food-box'>
-            <div className="food-card">
-              <img src={cheeseNan} alt="cheese Nan" />
-              <div className="set-card">
-                <h4 className="food-name">Chicken Curry</h4>
-                <p className="food-info">Typical chicken curry.</p>
-                <p className="food-info price">$650</p>
+      <Tabs defaultActiveKey="Tandoori" id="menu-sides-tab" className="mb-3">
+        {
+          sides.map((sideType, index) => (
+            <Tab eventKey={tabList[index]} title={sideType.name} key={sideType.name}>
+              <div className="food-box">
+                {
+                  sideType.items.map((side) => (
+
+                    <div className="food-card" key={side.name}>
+                      <img src={side.image} alt={`${side.name}`} />
+                      <div className="set-card">
+                        <h4 className="food-name">{side.name}</h4>
+                        <p className="food-info">{side.description}</p>
+                        <p className="food-info price">{side.price}</p>
+                      </div>
+                    </div>
+                  ))
+                }
               </div>
-            </div>
-            <div className="food-card">
-              <img src={cheeseNan} alt="cheese Nan" />
-              <div className="set-card">
-                <h4 className="food-name">Chicken Curry</h4>
-                <p className="food-info">Typical chicken curry.</p>
-                <p className="food-info price">$650</p>
-              </div>
-            </div>
-            <div className="food-card">
-              <img src={cheeseNan} alt="cheese Nan" />
-              <div className="set-card">
-                <h4 className="food-name">Chicken Curry</h4>
-                <p className="food-info">Lorem, od doloremque nesciunt commodi numquam amet nihil. Typical chicken curry.</p>
-                <p className="food-info price">$650</p>
-              </div>
-            </div>
-          </div>
-        </Tab>
-        <Tab eventKey="Salad" title="Salad">
-          <div className='food-box'>
-            <div className="food-card">
-              <img src={cheeseNan} alt="cheese Nan" />
-              <div className="set-card">
-                <h4 className="food-name">Chicken Curry</h4>
-                <p className="food-info">Typical chicken curry.</p>
-                <p className="food-info price">$650</p>
-              </div>
-            </div>
-            <div className="food-card">
-              <img src={cheeseNan} alt="cheese Nan" />
-              <div className="set-card">
-                <h4 className="food-name">Chicken Curry</h4>
-                <p className="food-info">Typical chicken curry.</p>
-                <p className="food-info price">$650</p>
-              </div>
-            </div>
-            <div className="food-card">
-              <img src={cheeseNan} alt="cheese Nan" />
-              <div className="set-card">
-                <h4 className="food-name">Chicken Curry</h4>
-                <p className="food-info">Lorem, od doloremque nesciunt commodi numquam amet nihil. Typical chicken curry.</p>
-                <p className="food-info price">$650</p>
-              </div>
-            </div>
-          </div>
-        </Tab>
-        <Tab eventKey="Appetizer" title="Appetizer">
-          <div className='food-box'>
-            <div className="food-card">
-              <img src={cheeseNan} alt="cheese Nan" />
-              <div className="set-card">
-                <h4 className="food-name">Chicken Curry</h4>
-                <p className="food-info">Typical chicken curry.</p>
-                <p className="food-info price">$650</p>
-              </div>
-            </div>
-            <div className="food-card">
-              <img src={cheeseNan} alt="cheese Nan" />
-              <div className="set-card">
-                <h4 className="food-name">Chicken Curry</h4>
-                <p className="food-info">Typical chicken curry.</p>
-                <p className="food-info price">$650</p>
-              </div>
-            </div>
-            <div className="food-card">
-              <img src={cheeseNan} alt="cheese Nan" />
-              <div className="set-card">
-                <h4 className="food-name">Chicken Curry</h4>
-                <p className="food-info">Lorem, od doloremque nesciunt commodi numquam amet nihil. Typical chicken curry.</p>
-                <p className="food-info price">$650</p>
-              </div>
-            </div>
-          </div>
-        </Tab>
+            </Tab>
+          ))
+        }
       </Tabs>
     </>
   );
